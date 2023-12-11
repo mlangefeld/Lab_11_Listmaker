d2b76b7e78c821a3e1fa1c23c092e2ecd261c3c6
mlangefeld <langefmn@mail.uc.edu>
2023-12-10 21:08:45 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,95 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner in = new Scanner(System.in);
+    static ArrayList<String> myArrList = new ArrayList<>();
+    public static void main(String[] args) {
+
+        boolean done = false;
+        String menuOpt;
+        int git;
+
+
+        do {
+            menuOpt = SafeInput.getRegExString(in, "Menu options: \nA (add): \nD (delete): \nP (print): \nQ (quit): ", "^[AaDdPpQq]$");
+            menuOpt = menuOpt.toUpperCase();
+
+
+
+
+            switch (menuOpt){
+
+                case "A":
+
+                    addItem();
+                    break;
+
+                case "D":
+
+                    deleteItem();
+                    break;
+
+                case "P":
+
+                    printList();
+                    break;
+
+                case "Q":
+
+                    quitProg();
+                    break;
+
+            }
+            System.out.println();
+
+
+        } while (!done);
+
+
+    }
+
+
+    private static void addItem(){
+
+        String add = SafeInput.getNonZeroLenStrings(in, "Add to array");
+        myArrList.add(add);
+    }
+
+    private static void deleteItem(){
+        int delete = SafeInput.getRangedInt(in, "Enter item number that you want deleted:", 1, myArrList.size());
+        String item = myArrList.remove(delete-1);
+        System.out.println("Succesfully deleted: " + item);
+
+    }
+
+    private static void quitProg(){
+        if (SafeInput.getYNConfirm(in, "Are you sure you want to quit? [Y,N]: ")) {
+            System.out.println("Exiting program...");
+            System.exit(0);
+        }
+    }
+
+    private static void printList(){
+        System.out.println();
+        if (myArrList.isEmpty()) {
+            System.out.println("There are no items on the list");
+        } else {
+            System.out.println("---- Current List ----");
+            for (int x = 0; x < myArrList.size(); x++) {
+                System.out.println((x + 1) + ". " + myArrList.get(x));
+
+            }
+        }
+
+    }
+
+    private static void displayItems(){
+
+        System.out.println("-----Current List-----");
+        for (int x = 0; x < myArrList.size(); x++) {
+
+            System.out.println((x + 1) + ". " + myArrList.get(x));
+        }
+    }
+}
---
+++ src/SafeInput.java
@@ -0,0 +1,202 @@
+import java.util.Scanner;
+
+public class SafeInput {
+
+    static Scanner in = new Scanner(System.in);
+    int git;
+
+    public static String getNonZeroLenStrings(Scanner pipe, String prompt) {
+        String retString = "";
+        do {
+            System.out.print("\n" + prompt + ": ");
+            retString = pipe.nextLine();
+        } while (retString.length() == 0);
+
+        return retString;
+    }
+
+    public static int getInt(Scanner pipe, String prompt) {
+
+        int randomInt = 0;
+
+        while (true) {
+
+            System.out.print(prompt + ": ");
+
+            if (pipe.hasNextInt()) {
+
+                randomInt = pipe.nextInt();
+                pipe.nextLine();
+                return randomInt;
+
+            } else {
+
+                System.out.println("Please enter an integer");
+                pipe.nextLine();
+
+            }
+
+        }
+
+    }
+
+    public static double getDouble(Scanner pipe, String prompt) {
+        double randomDouble;
+
+        while (true) {
+
+            System.out.print(prompt + ": ");
+
+            if (pipe.hasNextDouble()) {
+
+                randomDouble = pipe.nextDouble();
+                pipe.nextLine();
+                return randomDouble;
+
+            } else {
+
+                System.out.println("Please enter a number");
+                pipe.nextLine();
+            }
+
+        }
+    }
+
+    public static int getRangedInt(Scanner pipe, String prompt, int low, int high) {
+        int rangedInt;
+
+        while (true) {
+
+            System.out.print(prompt + " [" + low + " - " + high + "]: ");
+            if (pipe.hasNextInt()) {
+
+                rangedInt = pipe.nextInt();
+
+                if (rangedInt >= low && rangedInt <= high) {
+                    break;
+
+                } else {
+
+                    System.out.println("Please enter a number inbetween " + low + " and " + high + ".");
+                }
+
+            } else {
+
+                System.out.println("Invalid input. Please enter an integer.");
+                pipe.next();
+
+            }
+
+            pipe.nextLine();
+
+        }
+        return rangedInt;
+
+    }
+
+    public static double getRangedDouble(Scanner pipe, String prompt, double low, double high){
+        double rangedDouble;
+
+        while (true){
+
+            System.out.print(prompt + " [" + low + " - " + high + "]: ");
+
+            if (pipe.hasNextDouble()){
+
+                rangedDouble = pipe.nextDouble();
+
+                if (rangedDouble >= low && rangedDouble <= high){
+                    break;
+
+                } else {
+
+                    System.out.println("Please enter a number inbetween " + low + " and " + high + ".");
+                }
+
+            } else {
+
+                System.out.println("Invalid input. Please enter a number inbetween " + low + " and " + high + ".");
+                pipe.next();
+
+            }
+
+            pipe.nextLine();
+
+        }
+
+        return rangedDouble;
+
+    }
+
+
+
+    public static boolean getYNConfirm(Scanner pipe, String prompt) {
+        String answer = "";
+        //pipe.nextLine();
+
+        while (true){
+
+            System.out.print(prompt);
+            answer = pipe.nextLine();
+
+            if (answer.equalsIgnoreCase("y")){
+
+                return true;
+            } else if (answer.equalsIgnoreCase("n")) {
+
+                return false;
+
+            } else {
+
+                System.out.println("Please enter either Y or N");
+            }
+        }
+    }
+
+
+    public static String getRegExString(Scanner pipe, String prompt, String regEx){
+        String input;
+
+        do {
+
+            System.out.print(prompt);
+            input = pipe.nextLine().trim();
+
+            if (!input.matches(regEx)) {
+
+                System.out.println("Invalid input. Please try again.");
+
+            }
+
+        } while (!input.matches(regEx));
+
+        return input;
+    }
+
+
+    public static String prettyHeader(String msg){
+        final int totalWidth = 60;
+        final int borderWidth = 3;
+        StringBuilder header = new StringBuilder();
+
+        String borderLine = "*".repeat(totalWidth) + "\n";
+
+        header.append(borderLine);
+
+        int messageWidth = msg.length();
+        int totalPadding = totalWidth - (borderWidth * 2) - messageWidth;
+        int paddingSide = totalPadding / 2;
+
+        header.append("*".repeat(borderWidth));
+        header.append(" ".repeat(paddingSide));
+        header.append(msg);
+        header.append(" ".repeat(totalPadding - paddingSide));
+        header.append("*".repeat(borderWidth));
+        header.append("\n");
+        header.append(borderLine);
+
+        return header.toString();
+    }
+
+
+}
